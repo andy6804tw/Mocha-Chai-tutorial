4ac689e8ba461fb29dd5ba1aa03da37f7e093e71
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-03-16 10:03:47 +0800

    modify http listen

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-http.createServer(app)
-  .listen(3000, () => {
-    console.log('Example app listening on port 3000!');
-});
+if (!module.parent) {
+  http.createServer(app)
+    .listen(3000, () => {
+      console.log('Example app listening on port 3000!');
+    });
+}
 
 module.exports = app;
--- test/appTest.js
+++ test/appTest.js
@@ -1,14 +1,24 @@
 const assert = require('chai').assert;
 const expect = require('chai').expect;
 const supertest = require('supertest');
-const api = supertest('http://localhost:3000');
 
 const app = require('../app');
 
 
 describe('User', function () {
+  let request = null
+  let server = null
+  
+  before(function (done) {
+    server = app.listen(done)
+    request = supertest.agent(server)
+  })
+
+  after(function (done) {
+    server.close(done)
+  })
   it('should return a 200 response', function (done) {
-    api.get('/')
+    request.get('/')
       .set('Accept', 'application/json')
       .expect(200, done);
   })
